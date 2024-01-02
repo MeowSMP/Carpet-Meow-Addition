013fe651a8ca385f56abec068979c29aebd8f897
chuanchen <chuanchen@git.com>
2024-01-02 23:37:42 +0800

    假人管理器写完了（应该）

--- src/main/java/cn/meowsmp/carpetmeowaddition/command/PlayerManagerCommand.java
+++ src/main/java/cn/meowsmp/carpetmeowaddition/command/PlayerManagerCommand.java
@@ -2,6 +2,9 @@ package cn.meowsmp.carpetmeowaddition.command;
 
 import carpet.patches.EntityPlayerMPFake;
 import cn.meowsmp.carpetmeowaddition.util.fakeplayer.FakePlayerData;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -13,45 +16,148 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class PlayerManagerCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("playerManager").requires(source -> true)
-                .then(CommandManager.literal("add")
+                .then(CommandManager.literal("save")
                         .then(CommandManager.argument("player", EntityArgumentType.player())
-                                .executes(context -> add(context, null))
+                                .executes(context -> save(context, null))
                                 .then(CommandManager.argument("alias", StringArgumentType.string())
-                                        .executes(context -> add(context, StringArgumentType.getString(context, "alias"))))))
+                                        .executes(context -> save(context, StringArgumentType.getString(context, "alias"))))))
                 .then(CommandManager.literal("spawn")
                         .then(CommandManager.argument("name", StringArgumentType.string())
                                 .suggests(FakePlayerData.listSuggests())
                                 .executes(PlayerManagerCommand::spawn)))
+                .then(CommandManager.literal("delete")
+                        .then(CommandManager.argument("name", StringArgumentType.string())
+                                .suggests(FakePlayerData.listSuggests())
+                                .executes(PlayerManagerCommand::delete)))
+                .then(CommandManager.literal("supplement")
+                        .then(CommandManager.literal("annotation")
+                                .then(CommandManager.argument("name", StringArgumentType.string())
+                                        .suggests(FakePlayerData.listSuggests())
+                                        .then(CommandManager.argument("annotation", StringArgumentType.string())
+                                                .executes(PlayerManagerCommand::addAnnotation))))
+                        .then(CommandManager.literal("alias")
+                                .then(CommandManager.argument("name", StringArgumentType.string())
+                                        .suggests(FakePlayerData.listSuggests())
+                                        .then(CommandManager.argument("alias", StringArgumentType.string())
+                                                .executes(PlayerManagerCommand::addAlias)))))
+
+                .then(CommandManager.literal("info")
+                        .then(CommandManager.argument("info", StringArgumentType.string())
+                                .suggests(FakePlayerData.listSuggests())
+                                .executes(PlayerManagerCommand::showInfo)))
+                .then(CommandManager.literal("list")
+                        .executes(PlayerManagerCommand::list))
         );
     }
 
-    private static int add(CommandContext<ServerCommandSource> context, @Nullable String alias) throws CommandSyntaxException {
+
+    // 保存假玩家数据
+    @SuppressWarnings("SpellCheckingInspection")
+    private static int save(CommandContext<ServerCommandSource> context, @Nullable String alias) throws CommandSyntaxException {
         ServerPlayerEntity player = EntityArgumentType.getPlayer(context, "player");
         if (player instanceof EntityPlayerMPFake fakePlayer) {
             FakePlayerData fakePlayerData = new FakePlayerData(fakePlayer, alias, context);
             try {
-                FakePlayerData.save(fakePlayerData, context.getSource().getServer());
+                boolean resave = FakePlayerData.save(fakePlayerData, context.getSource().getServer());
+                context.getSource().sendMessage(Text.literal("已" + (resave ? "重新保存" : "保存")
+                        + (alias == null ? player.getName().getString() : alias) + "的玩家数据"));
                 return 1;
             } catch (IOException e) {
-                context.getSource().sendMessage(Text.literal("保存json文件时出现意外问题"));
+                context.getSource().sendMessage(Text.literal("无法保存假玩家数据"));
+            }
+        } else {
+            context.getSource().sendMessage(Text.literal(player.getName().getString() + "不是假玩家"));
+        }
+        return 0;
+    }
+
+    // 生成假玩家
+    private static int spawn(CommandContext<ServerCommandSource> context) {
+        String name = StringArgumentType.getString(context, "name");
+        try {
+            FakePlayerData.load(name, context, true);
+            return 1;
+        } catch (NullPointerException e) {
+            context.getSource().getServer().getPlayerManager().broadcast(Text.literal("无法从文件读取假玩家数据"), false);
+        }
+        return 0;
+    }
+
+    // 删除假玩家数据
+    private static int delete(CommandContext<ServerCommandSource> context) {
+        FakePlayerData.deleteFile(context);
+        return 1;
+    }
+
+    // 修改json文件
+    private static boolean modifyJsonFile(File file, String property, String value) throws IOException {
+        boolean hasProperty;
+        String json;
+        BufferedReader jsonReader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8));
+        // 读取旧的json文件的内容
+        try (jsonReader) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonObject jsonObject = gson.fromJson(jsonReader, JsonObject.class);
+            hasProperty = jsonObject.has(property);
+            // 为json文件添加新值
+            jsonObject.addProperty(property, value);
+            // 将json转化为字符串
+            json = gson.toJson(jsonObject);
+        }
+        // 将修改好的json重新写入本地文件
+        BufferedWriter jsonWriter = new BufferedWriter(new FileWriter(file, StandardCharsets.UTF_8));
+        try (jsonWriter) {
+            jsonWriter.write(json);
+        }
+        return hasProperty;
+    }
+
+    // 添加注释
+    private static int addAnnotation(CommandContext<ServerCommandSource> context) {
+        String name = StringArgumentType.getString(context, "name");
+        File file = FakePlayerData.load(name, context, false);
+        if (file != null) {
+            try {
+                boolean hasAnnotation = modifyJsonFile(file, "annotation", StringArgumentType.getString(context, "annotation"));
+                context.getSource().sendMessage(Text.literal(hasAnnotation ? "修改了" + name + "的注释" : "为" + name + "添加注释"));
+                return 1;
+            } catch (IOException e) {
+                context.getSource().sendMessage(Text.literal("尝试添加注释时出现意外问题"));
             }
         }
         return 0;
     }
 
-    private static int spawn(CommandContext<ServerCommandSource> context) {
+    // 添加别名
+    private static int addAlias(CommandContext<ServerCommandSource> context) {
         String name = StringArgumentType.getString(context, "name");
-        try {
-            FakePlayerData.load(name, context.getSource().getServer());
-            return 1;
-        } catch (IOException e) {
-            context.getSource().getServer().getPlayerManager().broadcast(Text.literal("加载json文件时出现意外问题"), false);
+        File file = FakePlayerData.load(name, context, false);
+        if (file != null) {
+            try {
+                boolean hasAlias = modifyJsonFile(file, "alias", StringArgumentType.getString(context, "alias"));
+                context.getSource().sendMessage(Text.literal(hasAlias ? "修改了" + name + "的别名" : "为" + name + "添加了别名"));
+            } catch (IOException e) {
+                context.getSource().sendMessage(Text.literal("尝试设置别名时出现意外问题"));
+            }
         }
         return 0;
+    }
+
+    // 显示玩家数据的信息
+    private static int showInfo(CommandContext<ServerCommandSource> context) {
+        FakePlayerData.showInfo(StringArgumentType.getString(context, "info"), context);
+        return 1;
+    }
+
+    // 列出所有玩家数据
+    private static int list(CommandContext<ServerCommandSource> context) {
+        FakePlayerData.list(context);
+        return 1;
     }
 }
--- src/main/java/cn/meowsmp/carpetmeowaddition/util/fakeplayer/FakePlayerData.java
+++ src/main/java/cn/meowsmp/carpetmeowaddition/util/fakeplayer/FakePlayerData.java
@@ -20,7 +20,11 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.WorldSavePath;
 import net.minecraft.util.math.Vec3d;
@@ -31,6 +35,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
@@ -47,7 +52,7 @@ public class FakePlayerData {
      * 假玩家的别名
      */
     @Nullable
-    private String alias;
+    private final String alias;
 
     /**
      * 该对象的创建者的玩家名
@@ -67,19 +72,19 @@ public class FakePlayerData {
     /**
      * 假玩家是否潜行
      */
-    private boolean sneaking;
+    private final boolean sneaking;
     /**
      * 假玩家所在的维度
      */
     @NotNull
-    private String dimension;
+    private final String dimension;
     /**
      * 假玩家的游戏模式
      */
     @Nullable
-    private String gamemode;
+    private final String gamemode;
 
-    private EntityPlayerActionPack playerActionPack;
+    private final EntityPlayerActionPack playerActionPack;
 
     public FakePlayerData(EntityPlayerMPFake fakePlayer, @Nullable String alias, CommandContext<ServerCommandSource> context) {
         ServerPlayerEntity player = context.getSource().getPlayer();
@@ -108,7 +113,12 @@ public class FakePlayerData {
         this.playerActionPack = ((ServerPlayerInterface) fakePlayer).getActionPack();
     }
 
-    public static void save(FakePlayerData fakePlayerData, MinecraftServer server) throws IOException {
+    /**
+     * 保存假玩家的数据
+     *
+     * @return 是否为重新保存（覆盖）
+     */
+    public static boolean save(FakePlayerData fakePlayerData, MinecraftServer server) throws IOException {
         JsonObject jsonProperty = new JsonObject();
         jsonProperty.addProperty("name", fakePlayerData.name);
         jsonProperty.addProperty("alias", fakePlayerData.alias);
@@ -148,43 +158,74 @@ public class FakePlayerData {
             jsonAction.add("use", jsonUse);
         }
         jsonProperty.add("action", jsonAction);
+
         // 将假玩家数据以UTF-8编码写入本地文件
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(getFile(server),
-                fakePlayerData.name + FakePlayerData.EXTENSION), StandardCharsets.UTF_8))) {
+        File file = new File(getFile(server), fakePlayerData.name + FakePlayerData.EXTENSION);
+        boolean exists = file.exists();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, StandardCharsets.UTF_8))) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             bw.write(gson.toJson(jsonProperty));
         }
+        return exists;
     }
 
-    public static void load(String playerName, MinecraftServer server) throws IOException {
+    // 加载假玩家
+    public static File load(String playerName, CommandContext<ServerCommandSource> context, boolean spawn) {
+        MinecraftServer server = context.getSource().getServer();
         File[] files = getFile(server).listFiles();
         if (files != null) {
             for (File file : files) {
-                BufferedReader readJson = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8));
-                try (readJson) {
-                    Gson gson = new Gson();
-                    JsonObject jsonObject = gson.fromJson(readJson, JsonObject.class);
-                    // 判断假玩家的名称或别名是否与文件中的匹配
-                    if ((jsonObject.has("alias") && Objects.equals(playerName, jsonObject.get("alias").getAsString())) ||
-                            (jsonObject.has("name") && Objects.equals(playerName, jsonObject.get("name").getAsString()))) {
-                        // 生成假玩家
-                        createFake(jsonObject, server);
-                        return;
-                    }
+                // 如果文件扩展名不是.json，直接跳过
+                if (!file.getName().endsWith(FakePlayerData.EXTENSION)) {
+                    continue;
                 }
+                BufferedReader readJson;
+                try {
+                    readJson = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8));
+                    try (readJson) {
+                        Gson gson = new Gson();
+                        JsonObject jsonObject = gson.fromJson(readJson, JsonObject.class);
+                        // 判断假玩家的名称或别名是否与文件中的匹配
+                        if (isDesignatedFile(playerName, jsonObject)) {
+                            if (spawn) {
+                                // 生成假玩家
+                                createFake(jsonObject, context);
+                            }
+                            return file;
+                        }
+                    }
+                } catch (IOException e) {
+                    CarpetMeowAddition.LOGGER.warn("处理名为" + file.getName() + "的文件时出现意外问题");
+                }
+
             }
         }
         server.getPlayerManager().broadcast(Text.literal("找不到名为" + playerName + "的玩家信息"), false);
+        return null;
     }
 
-    private static void createFake(JsonObject jsonObject, MinecraftServer server) {
+    // 判断当前文件是否为保存了指定玩家信息的文件
+    private static boolean isDesignatedFile(String playerName, JsonObject jsonObject) {
+        return (jsonObject.has("alias") && Objects.equals(playerName, jsonObject.get("alias").getAsString())) ||
+                (jsonObject.has("name") && Objects.equals(playerName, jsonObject.get("name").getAsString()));
+    }
+
+    // 创造假玩家
+    private static void createFake(JsonObject jsonObject, CommandContext<ServerCommandSource> context) {
+        MinecraftServer server = context.getSource().getServer();
         // 假玩家名
-        JsonObject playerPos = jsonObject.get("playerPos").getAsJsonObject();
+        String username = jsonObject.get("name").getAsString();
+        if (server.getPlayerManager().getPlayer(username) != null) {
+            // 玩家已存在
+            context.getSource().sendMessage(Text.literal("玩家" + username + "已存在"));
+            return;
+        }
         // 假玩家的位置
+        JsonObject playerPos = jsonObject.get("playerPos").getAsJsonObject();
         Vec3d pos = new Vec3d(playerPos.get("x").getAsDouble(), playerPos.get("y").getAsDouble(), playerPos.get("z").getAsDouble());
         JsonObject playerDirection = jsonObject.get("direction").getAsJsonObject();
         // 生成假玩家
-        EntityPlayerMPFake fakePlayer = EntityPlayerMPFake.createFake(jsonObject.get("name").getAsString(), server, pos,
+        EntityPlayerMPFake fakePlayer = EntityPlayerMPFake.createFake(username, server, pos,
                 playerDirection.get("yaw").getAsDouble(), playerDirection.get("pitch").getAsDouble(),
                 getWorld(jsonObject.get("dimension").getAsString()),
                 getGameMode(jsonObject.get("gamemode").getAsString()), false);
@@ -220,6 +261,33 @@ public class FakePlayerData {
         }
     }
 
+    // 删除假玩家数据
+    public static void deleteFile(CommandContext<ServerCommandSource> context) {
+        String playerName = StringArgumentType.getString(context, "name");
+        File[] files = getFile(context).listFiles();
+        if (files != null) {
+            for (File file : files) {
+                try (BufferedReader jsonReader = new BufferedReader(new FileReader(file))) {
+                    Gson gson = new Gson();
+                    JsonObject jsonObject = gson.fromJson(jsonReader, JsonObject.class);
+                    if (isDesignatedFile(playerName, jsonObject)) {
+                        // 删除文件前关闭io流
+                        jsonReader.close();
+                        boolean deleted = file.delete();
+                        context.getSource().sendMessage(Text.literal((deleted ? "已" : "无法") + "删除" + playerName + "的玩家数据"));
+                        return;
+                    }
+                } catch (FileNotFoundException e) {
+                    CarpetMeowAddition.LOGGER.warn("找不到名为" + file.getName() + "的文件");
+                } catch (IOException e) {
+                    CarpetMeowAddition.LOGGER.warn("处理文件" + file.getName() + "时出现意外问题");
+                }
+            }
+        }
+        context.getSource().sendMessage(Text.literal("找不到名为" + playerName + "的玩家信息"));
+    }
+
+    // 在命令中列出建议
     public static SuggestionProvider<ServerCommandSource> listSuggests() {
         return (context, builder)
                 -> CommandSource.suggestMatching(Arrays.stream(Objects.requireNonNull(getFile(context.getSource().getServer()).listFiles()))
@@ -240,6 +308,220 @@ public class FakePlayerData {
                     }
                     return "";
                 }).map(StringArgumentType::escapeIfRequired), builder);
+    }
+
+    // 显示假玩家数据的详细信息
+    public static void showInfo(String username, CommandContext<ServerCommandSource> context) {
+        File[] files = getFile(context).listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (!file.getName().endsWith(FakePlayerData.EXTENSION)) {
+                    continue;
+                }
+                try {
+                    BufferedReader jsonReader = new BufferedReader(new FileReader(file));
+                    try (jsonReader) {
+                        Gson gson = new Gson();
+                        JsonObject jsonObject = gson.fromJson(jsonReader, JsonObject.class);
+                        if (isDesignatedFile(username, jsonObject)) {
+                            showInfo(context, jsonObject);
+                            return;
+                        }
+                    } catch (IOException e) {
+                        CarpetMeowAddition.LOGGER.warn("处理文件" + file.getName() + "时出现意外问题");
+                    }
+                } catch (FileNotFoundException e) {
+                    CarpetMeowAddition.LOGGER.warn("找不到名为" + file.getName() + "的文件");
+                }
+            }
+        }
+        context.getSource().sendMessage(Text.literal("找不到玩家" + username + "的数据信息"));
+    }
+
+    private static void showInfo(CommandContext<ServerCommandSource> context, JsonObject jsonObject) {
+        // 一行代码实现功能，一百行代码防小天才，每个元素都判断是否存在
+        ArrayList<MutableText> list = new ArrayList<>();
+        // 假玩家名
+        if (jsonObject.has("name")) {
+            list.add(Text.literal("名称：" + jsonObject.get("name").getAsString()));
+        }
+        // 假玩家别名
+        if (jsonObject.has("alias")) {
+            list.add(Text.literal("别名：" + jsonObject.get("alias").getAsString()));
+        }
+        // 创建者玩家名
+        if (jsonObject.has("creator")) {
+            list.add(Text.literal("创建者：" + jsonObject.get("creator").getAsString()));
+        }
+        // 假玩家位置
+        if (jsonObject.has("playerPos")) {
+            JsonObject jsonPos = jsonObject.get("playerPos").getAsJsonObject();
+            if (jsonPos.has("x") && jsonPos.has("y") && jsonPos.has("z")) {
+                list.add(Text.literal("位置：["
+                        + String.format("%.2f", jsonPos.get("x").getAsDouble()) + ", "
+                        + String.format("%.2f", jsonPos.get("x").getAsDouble()) + ", "
+                        + String.format("%.2f", jsonPos.get("x").getAsDouble()) + "]"
+                ));
+            }
+        }
+        // 假玩家朝向
+        if (jsonObject.has("direction")) {
+            JsonObject jsonPos = jsonObject.get("direction").getAsJsonObject();
+            if (jsonPos.has("yaw") && jsonPos.has("pitch")) {
+                list.add(Text.literal("朝向："
+                        + String.format("%.2f", jsonPos.get("yaw").getAsDouble()) + ", "
+                        + String.format("%.2f", jsonPos.get("pitch").getAsDouble())
+                ));
+            }
+        }
+        // 假玩家是否潜行
+        if (jsonObject.has("sneaking")) {
+            list.add(Text.literal("潜行：" + jsonObject.get("sneaking").getAsBoolean()));
+        }
+        // 假玩家所在维度
+        if (jsonObject.has("dimension")) {
+            String dimensionId = jsonObject.get("dimension").getAsString();
+            switch (dimensionId) {
+                case "minecraft:overworld", "overworld" -> list.add(Text.literal("维度：主世界"));
+                case "minecraft:the_nether", "the_nether" -> list.add(Text.literal("维度：下界"));
+                case "minecraft:the_end", "the_end" -> list.add(Text.literal("维度：末地"));
+                default -> list.add(Text.literal("维度：" + dimensionId));
+            }
+        }
+        // 假玩家游戏模式
+        if (jsonObject.has("gamemode")) {
+            String gameMode = jsonObject.get("gamemode").getAsString();
+            switch (gameMode) {
+                case "survival" -> list.add(Text.literal("游戏模式：生存模式"));
+                case "creative" -> list.add(Text.literal("游戏模式：创造模式 "));
+                case "adventure" -> list.add(Text.literal("游戏模式：冒险模式"));
+                case "spectator" -> list.add(Text.literal("游戏模式：旁观模式"));
+                default -> list.add(Text.literal("游戏模式：" + gameMode));
+            }
+        }
+        // 假玩家动作
+        if (jsonObject.has("action")) {
+            JsonObject action = jsonObject.get("action").getAsJsonObject();
+            list.add(Text.literal("动作："));
+            // 左键
+            if (action.has("attack")) {
+                JsonObject attack = action.get("attack").getAsJsonObject();
+                if (attack.has("continuous") && attack.get("continuous").getAsBoolean()) {
+                    list.add(Text.literal("    左键：长按"));
+                } else if (attack.has("interval")) {
+                    list.add(Text.literal("    左键：间隔" + attack.get("interval").getAsInt() + "gt"));
+                }
+            }
+            // 右键
+            if (action.has("use")) {
+                JsonObject use = action.get("use").getAsJsonObject();
+                if (use.has("continuous") && use.get("continuous").getAsBoolean()) {
+                    list.add(Text.literal("    右键：长按"));
+                } else if (use.has("interval")) {
+                    list.add(Text.literal("    右键：间隔" + use.get("interval").getAsInt() + "gt"));
+                }
+            }
+        }
+        // （假玩家数据的）注释
+        if (jsonObject.has("annotation")) {
+            list.add(Text.literal("注释：" + jsonObject.get("annotation").getAsString()));
+        }
+        // 发送消息
+        for (MutableText text : list) {
+            context.getSource().sendMessage(text);
+        }
+    }
+
+    public static void list(CommandContext<ServerCommandSource> context) {
+        File[] files = getFile(context).listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (!file.getName().endsWith(FakePlayerData.EXTENSION)) {
+                    continue;
+                }
+                try {
+                    BufferedReader jsonReader = new BufferedReader(new FileReader(file));
+                    try (jsonReader) {
+                        Gson gson = new Gson();
+                        JsonObject jsonObject = gson.fromJson(jsonReader, JsonObject.class);
+                        eachPlayerData(context, jsonObject);
+                    } catch (IOException e) {
+                        CarpetMeowAddition.LOGGER.warn("处理文件" + file.getName() + "时出现意外问题");
+                    }
+                } catch (FileNotFoundException e) {
+                    CarpetMeowAddition.LOGGER.warn("找不到名为" + file.getName() + "的文件");
+                }
+            }
+        }
+    }
+
+    private static void eachPlayerData(CommandContext<ServerCommandSource> context, JsonObject jsonObject) {
+        ArrayList<Object> list = new ArrayList<>();
+        // 假玩家别名
+        String playerAlias = null;
+        // 假玩家名
+        String playerName = null;
+        // 为假玩家名别名赋值
+        if (jsonObject.has("alias")) {
+            playerAlias = jsonObject.get("alias").getAsString();
+        }
+        // 为假玩家本命赋值
+        if (jsonObject.has("name")) {
+            playerName = jsonObject.get("name").getAsString();
+        }
+        if (playerAlias == null && playerName == null) {
+            return;
+        }
+        // 定义假玩家登录和下线的命令
+        String loggedOutCommand = "/player " + playerName + " kill";
+        String loggedInCommand = "/playerManager spawn " + (playerAlias == null ? playerName :
+                StringArgumentType.escapeIfRequired(playerAlias));
+        // 点击上线
+        MutableText loggedIn = Text.literal("[↑]").styled(style -> style
+                // 点击执行命令
+                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, loggedInCommand))
+                // 添加显示文本
+                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("点击上线")))
+                // 设置为绿色
+                .withColor(Formatting.GREEN));
+        list.add(loggedIn);
+        list.add(" ");
+        // 点击下线
+        MutableText loggedOut = Text.literal("[↓]").styled(style -> style
+                // 点击执行命令
+                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, loggedOutCommand))
+                // 添加显示文本
+                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("点击下线")))
+                // 设置为红色
+                .withColor(Formatting.RED));
+        list.add(loggedOut);
+        list.add(" ");
+        // 在假玩家名上添加注释，如果没有注释，但是有别名，则添加本命
+        MutableText name = Text.literal(playerAlias == null ? playerName : playerAlias);
+        if (jsonObject.has("annotation")) {
+            name.styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                    Text.literal(jsonObject.get("annotation").getAsString()))));
+        } else if (playerAlias != null) {
+            MutableText nameText = Text.literal(playerName);
+            name.styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                    nameText)));
+        }
+        list.add(name);
+        context.getSource().sendMessage(appendAll(list));
+    }
+
+    private static MutableText appendAll(ArrayList<Object> list) {
+        MutableText mutableText = Text.literal("");
+        for (Object object : list) {
+            if (object instanceof String str) {
+                mutableText.append(Text.literal(str));
+            } else if (object instanceof Text text) {
+                mutableText.append(text);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        return mutableText;
     }
 
 
@@ -268,6 +550,11 @@ public class FakePlayerData {
      */
     private static GameMode getGameMode(String gameMode) {
         return GameMode.byName(gameMode);
+    }
+
+    // 获取文件
+    private static File getFile(CommandContext<ServerCommandSource> context) {
+        return getFile(context.getSource().getServer());
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
